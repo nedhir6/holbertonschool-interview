96aef3bf7ed3bb6dec1e28b14e337aca6fd6c78f
nedhir6 <nedhirb@gmail.com>
2021-08-04 22:18:38 +0100

    binary trees

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -9,13 +9,13 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *newn;
-newn = malloc(sizeof(binary_tree_t));
-if (newn == NULL)
+struct binary_tree_s *new;
+new = malloc(sizeof(binary_tree_t));
+if (new == NULL)
 return (NULL);
-newn->n = value;
-newn->parent = parent;
-newn->left = NULL;
-newn->right = NULL;
-return (newn);
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = NULL;
+return (new);
 }
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,6 +1,5 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-
 #include <stddef.h>
 
 /**
@@ -20,6 +19,6 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
-typedef struct binary_tree_s heap_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
