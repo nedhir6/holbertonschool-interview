6fda498cdfe402a75343b3cf38660d875e15332f
nedhir6 <nedhirb@gmail.com>
2021-08-26 23:42:53 +0100

    [Fix] Betty style

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -10,15 +10,15 @@
  */
 int is_palindrome(listint_t **head)
 {
-const listint_t *list,*h = *head;
-int *arr ,size_list = 0, n = 0;
+const listint_t *list, *h = *head;
+int *arr, size_list = 0, n = 0;
 list = h;
 while (list != NULL)
 {
 list = list->next;
 size_list++;
 }
-arr = malloc(size_list * sizeof(int));
+arr = malloc(size_list *sizeof(int));
 list = h;
 while (list != NULL)
 {
@@ -26,10 +26,10 @@ arr[n] = list->n;
 list = list->next;
 n++;
 }
-for(n = 0; n <= size_list / 2 && size_list != 0; n++)
+for (n = 0; n <= size_list / 2 && size_list != 0; n++)
 {
 if (arr[n] != arr[size_list - n])
-return(1);
+return (1);
 }
-return(0);
+return (0);
 }
