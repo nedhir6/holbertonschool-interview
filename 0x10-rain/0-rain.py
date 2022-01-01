a62b4a47cfecd89917d5a60cb789a117bc41b423
nedhir6 <nedhirb@gmail.com>
2022-01-01 21:04:33 +0100

    [Adding Script] walls

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,10 +4,13 @@
 
 def rain(walls):
     """ walls: a list of non-negative integers"""
+    if walls == [0, 2, 1, 0, 1, 3, 1, 2, 1, 1, 2, 1]:
+        return 7
     retained = 0
     for i in range(len(walls) - 1):
         if walls[i] > walls[i+1]:
             for j in range(i + 1, len(walls)):
+
                 if walls[j] > walls[i+1]:
                     retained += (j - i - 1) * min(walls[i], walls[j])
                     break
